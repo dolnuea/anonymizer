3bd7202a35ba0142bd1ca2787e98c1d5b40a4c99
Luna Dagci <ldagci@albany.edu>
2021-10-29 10:04:27 -0400

    All commands work

--- client/client_udp.py
+++ client/client_udp.py
@@ -37,7 +37,7 @@ PORT = int(sys.argv[2])  # 4455
 ADDR = (IP, PORT)
 FORMAT = "utf-8"
 SIZE = 1024
-download_dir = os.getcwd()  # download in working dir
+# download_dir = os.getcwd()  # download in working dir
 
 
 def main():
@@ -47,40 +47,68 @@ def main():
     """ Connecting to the server. """
     client.connect(ADDR)
 
+    """Continue getting input from the user until user quits"""
     while True:
 
         user_input = input("Enter command:")
+        """split the command into parts"""
         split_input = user_input.split()
+        """get the command"""
         command = split_input[0]
 
-        client.sendall(user_input.encode(FORMAT))  # send command
+        """send the commands to the Server"""
+        client.sendall(user_input.encode(FORMAT))
 
         print("Awaiting server response.")
 
-        if command == 'get'.casefold():
+        """Upload the file into the server"""
+        if command == 'PUT'.casefold():
 
-            output_filename = split_input[1]  # extract the output file name from the command
+            """get the file name from the command line argument"""
+            input_filename = split_input[1]
 
-            # download a file created by server:
-            #  https://stackoverflow.com/questions/29110620/how-to-download-file-from-local-server-in-python
+            """open file in read permission"""
+            file = open(input_filename, "r")
+            """read the contents in the original file"""
+            content = file.read()
+            file_size = os.path.getsize(input_filename)
+            """send the file size over to the server"""
+            client.sendall(str(file_size).encode(FORMAT))
 
+            """send the contents of the input file to the server"""
+            client.sendall(content.encode(FORMAT))
+
+            """ Closing the file. """
+            file.close()
+
+        # Download the file from the Server
+        elif command == 'GET'.casefold():
+
+            """extract the output file name from the command line arguments"""
+            output_filename = split_input[1]
+
+            """open the output file in write permission: if DNE then create a new file"""
             output_file = open(output_filename, "w+")
 
+            """receive the filesize from the server"""
             file_size = client.recv(SIZE).decode(FORMAT)
-
             print(file_size)
 
+            """receive the content if file from the server"""
             data = client.recv(int(file_size)).decode(FORMAT)
-
+            """write the contents of file into the file"""
             output_file.write(data)
+            """close the file"""
             output_file.close()
 
+        # quit the program and close connection
         elif command == 'quit'.casefold():
             print("Exiting program!")
             exit(0)
             break
 
-        print(client.recv(SIZE).decode(FORMAT))  # receive the message from the server
+        """receive the message from the server"""
+        print(client.recv(SIZE).decode(FORMAT))
 
     client.close()
 
--- server/server_udp.py
+++ server/server_udp.py
@@ -4,6 +4,10 @@ References
 client sends file to server, server saves the file and anonymizes it
 https://github.com/preetha2711/Stop-and-Wait-Protocol
 https://github.com/nikhilroxtomar/Stop-and-Wait-Protocol-Implemented-in-UDP-C
+https://stackoverflow.com/questions/15909064/python-implementation-for-stop-and-wait-algorithm
+https://stackoverflow.com/questions/5343358/stop-and-wait-socket-programming-with-udp#:~:text=The%20stop%20and%20wait%20protocol,before%20sending%20the%20next%20packet.
+https://github.com/mj2266/stop-and-wait-protocol
+https://github.com/z9z/reliable_data_transfer
 """
 import os
 import socket
@@ -14,24 +18,27 @@ PORT = int(sys.argv[1])  # 4450
 ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
-download_dir = os.getcwd()  # download in same folder (working directory
 
 
-# should I send the input file from client to server too? or is it ok to assume the file already exists theer?
+# download_dir = os.getcwd()  # download in same folder (working directory
+
+
 def main():
-    print("ip is " + socket.gethostbyname(socket.gethostname()))
+    print("IP: " + socket.gethostbyname(socket.gethostname()))
     print("[STARTING] Server is starting.")
+
     """ Staring a TCP socket. """
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     """ Bind the IP and PORT to the server. """
     server.bind(ADDR)
 
+    """Set server timeout"""
+    server.settimeout(0.5)
+
     """ Server is listening, i.e., server is now waiting for the client to connected. """
     server.listen()
     print("[LISTENING] Server is listening.")
-
-    data = None
 
     """ Server has accepted the connection from the client. """
     server, addr = server.accept()
@@ -43,27 +50,47 @@ def main():
         user_input = server.recv(SIZE)
         user_input = user_input.decode(FORMAT)
         print(user_input)
+
         user_input = user_input.split()
         command = user_input[0]
 
+        """Upload the file into the server from the Client"""
         if command == 'PUT'.casefold():
-            input_filename = user_input[1]
 
-            """ Opening and reading the file data. """
-            input_file = open(input_filename, "r")
-            data = input_file.read()
-            print(f"[RECV] File uploaded.")
-            message = "Server response: File uploaded."
+            """Get the file name from the command"""
+            input_filename = user_input[1]
+            """Open the file with write permission"""
+            input_file = open(input_filename, "w+")
+
+            """Receive the filesize from the client"""
+            file_size = server.recv(SIZE).decode(FORMAT)
+            # print(file_size)
+
+            """Receive the contents of the original file from the server"""
+            data = server.recv(int(file_size)).decode(FORMAT)
+            """Write the contents of the original file into input file"""
+            input_file.write(data)
+            """Close the file"""
             input_file.close()
 
+            # """ Opening and reading the file data. """
+            # input_file = open(input_filename, "r")
+            # data = input_file.read()
+            print(f"[RECV] File uploaded.")
+            message = "Server response: File uploaded."
+            # input_file.close()
+
+        # Anonymize the file
         elif command == 'keyword'.casefold():
 
             """ Receiving Keyword """
             keyword = user_input[1]
 
+            """Generate the output file name"""
             output_filename = remove_suffix(input_filename, '.txt') + '_anon.txt'
             print(output_filename)
 
+            """Open the output file with write permission"""
             output_file = open(output_filename, 'w+')
 
             """ Anonymizing File """
@@ -79,20 +106,19 @@ def main():
 
         elif command == 'GET'.casefold():
 
+            """Get the output file name from the command"""
             output_filename = user_input[1]
 
-            # https://stackoverflow.com/questions/29110620/how-to-download-file-from-local-server-in-python
-
+            """Open the output file with write permission"""
             output_file = open(output_filename, "r")  # open file in read format
 
+            """Send the file size to the Client"""
             file_size = os.path.getsize(output_filename)
-
             print(file_size)
-
             server.sendall(str(file_size).encode(FORMAT))  # send the file size
 
+            """Send over the contents if the output file to the Client"""
             content = output_file.read()  # read the contents in the anonymized file
-
             server.sendall(content.encode(FORMAT))  # send the contents of the output file to the client
 
             """ Closing the file. """
@@ -107,7 +133,6 @@ def main():
             break
 
         server.sendall(message.encode(FORMAT))  # send message to client
-
     server.close()
 
 
@@ -118,23 +143,49 @@ def remove_suffix(input_string, suffix):
     return input_string
 
 
-def send_ack(user_input, addr, socket):
-    global ACK
+"""
+ After every send, you wait for an acknowledgement message
+ to come in, and every time you receive, you send an 
+ acknowledgement.
+"""
+
+
+def receive_ack(user_input, addr, socket):
+    """
+    Data Sender receives the acknowledgement
+    :param user_input:
+    :param addr:
+    :param socket:
+    :return:
+    References:
+    https://www.isi.edu/nsnam/DIRECTED_RESEARCH/DR_HYUNAH/D-Research/stop-n-wait.html
+    https://stackoverflow.com/questions/15909064/python-implementation-for-stop-and-wait-algorithm
+    """
 
     while user_input:
         socket.sendto(bytes(user_input), addr)
         acknowledged = False
         while not acknowledged:
             try:
+                """After transmitting one packet, the sender waits for an 
+                acknowledgment (ACK) from the receiver before transmitting
+                 the next one"""
                 ACK, address = socket.recvfrom(SIZE)
                 acknowledged = True
             except socket.timeout:
+                """If the sender doesn't receive ACK for previous sent packet
+                 after a certain period of time, the sender times out and 
+                 retransmits that packet again."""
                 socket.sendto(bytes(user_input), addr)
         print(ACK)
         user_input = input()
 
 
-def receive_ack():
+def send_ack():
+    """
+    Data Receiver sends the acknowledgement
+    :return:
+    """
     pass
 
 
